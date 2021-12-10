889b364e0b307aa4ec5fbb007e4ce11b55bda18b
Lionel Moussavou <82570441+LionelMoussavou@users.noreply.github.com>
2021-12-10 15:35:00 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="style_index.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <header>
+            <?php
+                include 'header.html';
+            ?>
+        </header>
+<div class="container1">
+            <section class="hero">
+                <div class="titre">
+                    <h2>La promesse du meilleur de l'ancien et du <br>
+                    &nbsp nouveau monde de la protection social </h2>
+                </div>
+                <div class="choix">
+                    <a href="particulier.php"><button class="btn1">Artisan, <br> commerçant, <br>Profession libérale </button></a>
+                   <a href="entreprise.php"><button class="btn2">Dirigeant, <br> d'entreptise et ses, <br>salariés </button></a> 
+                </div>
+            </section>
+</div>
+<?php
+
+
+?>
+        <footer>
+            <?php include 'footer.html';
+            ?>
+        </footer>
+   
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>        
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+</body>  
+</html>
